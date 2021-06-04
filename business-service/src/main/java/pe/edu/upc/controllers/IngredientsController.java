13b438614a7a5f4c50a816886c23ece93b4c0b92
William <wmavalle@gmail.com>
2021-06-04 00:37:07 -0500

    bdd

--- business-service/src/main/java/pe/edu/upc/controllers/IngredientsController.java
+++ business-service/src/main/java/pe/edu/upc/controllers/IngredientsController.java
@@ -1,6 +1,5 @@
 package pe.edu.upc.controllers;
 
-import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,7 +15,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@Api
 @RequestMapping("/ingredients")
 public class IngredientsController {
     @Autowired
--- business-service/src/main/java/pe/edu/upc/controllers/MenuController.java
+++ business-service/src/main/java/pe/edu/upc/controllers/MenuController.java
@@ -1,6 +1,5 @@
 package pe.edu.upc.controllers;
 
-import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,7 +14,6 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
-@Api
 @RequestMapping("/menu")
 public class MenuController {
     @Autowired
