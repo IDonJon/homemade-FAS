404b9995cdec6e59bed44a129fd6875e24d04f26
William <wmavalle@gmail.com>
2021-06-13 09:37:34 -0500

    BDD

---
+++ security-service/src/test/java/DemoStepdefs.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class DemoStepdefs {
+    @Given("I perform GET operation for the {string}")
+    public void iPerformGETOperationForThe(String arg0) {
+    }
+
+    @And("I perform GET all menus")
+    public void iPerformGETAllMenus() {
+    }
+
+    @Then("I get {string} menus as a result")
+    public void iGetMenusAsAResult(String arg0) {
+    }
+}
